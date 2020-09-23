9f51be94e2ef886d985d4dfed65af7445a717044
Saul Baizman <saul.baizman@massart.edu>
2020-09-23 00:35:33 -0400

    Re-engineered quiz to add an icon next to the answer.

--- q.js
+++ q.js
@@ -1,6 +1,6 @@
 let correct_answer_count = 0 ;
 let answered_question_count = 0 ;
-let total_question_count = jQuery('ul.answers').length ; // FIXME
+let total_question_count = jQuery('div.answers').length ; // FIXME
 // console.log ('total_question_count:',total_question_count) ;
 
 jQuery(document).ready ( function ( ) {
@@ -10,65 +10,76 @@ jQuery(document).ready ( function ( ) {
         if ( jQuery('#quiz_selector').val() != '0' ) {
             // console.log ('#quiz_selector',jQuery('#quiz_selector').val());
             const url = new URL ( window.location ) ;
-            url.searchParams.set( 'quizzy', jQuery('#quiz_selector').val()) ;
+            url.searchParams.set( 'quiz', jQuery('#quiz_selector').val()) ;
             window.location = url ;
         }
     } ) ;
 
-jQuery('ul.answers li').on("mouseenter mouseleave", function (event) {
-    jQuery(this).toggleClass("hover");
+    /* Mouse-over state toggle. */
+    jQuery('div.answers div.answer').on("mouseenter mouseleave", function (event) {
+        jQuery(this).toggleClass("hover");
+    });
 
-        });
+    /* When clicked, check if an answer is correct. */
+    jQuery('div.answers div.answer').on('click', function (event) {
+        // console.log('clicked answer');
+        answered_question_count += 1 ;
+        // var class = jQuery(this).attr("class");
+        let classes = this.classList;
+        // console.log('classes',class);
+        // console.log('classes', classes.length);
+        // console.log('classes', classes.values());
 
-        jQuery('ul.answers li').on('click', function (event) {
-            console.log('clicked answer');
-            answered_question_count += 1 ;
-            // var class = jQuery(this).attr("class");
-            let classes = this.classList;
-            // console.log('classes',class);
-            // console.log('classes', classes.length);
-            // console.log('classes', classes.values());
+        let question_class = classes[0];
+        console.log ("question_class:",question_class) ;
+        let is_correct_answer = false ;
+        var iterator = classes.values();
 
-            let question_class = classes[0];
-
-            let is_correct_answer = false ;
-            var iterator = classes.values();
-
-            for(var value of iterator) {
-                if ( value == 'is_correct' ) {
-                    is_correct_answer = true ;
-                    break ;
-                }
+        for(var value of iterator) {
+            if ( value == 'is_correct' ) {
+                is_correct_answer = true ;
+                break ;
             }
+        }
 
-            if (is_correct_answer) {
-                jQuery(this).addClass('correct_answer');
-                correct_answer_count += 1;
-            } else {
-                jQuery(this).addClass('incorrect_answer');
-                // find the correct answer
-                jQuery('li.' + question_class + '.is_correct').addClass('correct_answer_not_selected');
-            }
+        if (is_correct_answer) {
+            jQuery(this).addClass('correct_answer');
+            let div_classes = jQuery(this).attr('class') ;
+            let icon_selector = 'div.'+div_classes.replace(/ /g,'.')+'+div.icon' ;
+            console.log('icon_selector:',icon_selector) ;
+            jQuery(icon_selector).html('&#10004;');
+            jQuery(icon_selector).addClass('correct');
+            // console.log('this class:',jQuery(this).attr('class')) ;
+            correct_answer_count += 1;
+        } else {
+            jQuery(this).addClass('incorrect_answer');
+            let div_classes = jQuery(this).attr('class') ;
+            let icon_selector = 'div.'+div_classes.replace(/ /g,'.')+'+div.icon' ;
+            jQuery(icon_selector).html('&#10008;');
+            jQuery(icon_selector).addClass('incorrect');
+            // find the correct answer
+            jQuery('div.' + question_class + '.is_correct').addClass('correct_answer_not_selected');
+        }
 
-            // Get rid of the hover class.
-            // jQuery(this).removeClass('hover');
+        // Get rid of the hover class.
+        // jQuery(this).removeClass('hover');
 
-            console.log('correct_answer_count:', correct_answer_count);
+        console.log('correct_answer_count:', correct_answer_count);
 
-            console.log('question class:', question_class);
-            // remove event handler.
-            jQuery('li.' + question_class).off('click');
-            jQuery('li.' + question_class).off('mouseenter');
-            jQuery('li.' + question_class).off('mouseleave');
+        console.log('question class:', question_class);
+        // remove event handler.
+        jQuery('div.' + question_class).off('click');
+        jQuery('div.' + question_class).off('mouseenter');
+        jQuery('div.' + question_class).off('mouseleave');
 
-            // is this the last question?
-            // if so, display a summary.
-            if ( answered_question_count == total_question_count ) {
-                jQuery('section#main').append ('<p class="summary">You got ' + correct_answer_count + ' out of ' + total_question_count + ' questions correct. Hooray!</p>') ;
-jQuery('body')[0].scrollIntoView(false);
+        // is this the last question?
+        // if so, display a summary.
+        if ( answered_question_count == total_question_count ) {
+            jQuery('section#main').append ('<p class="summary">You got ' + correct_answer_count + ' out of ' + total_question_count + ' questions correct. Hooray!</p>') ;
+            jQuery('body')[0].scrollIntoView(false);
 
-}
+        }
 
-});
+    });
 
 } ) ;
--- q.php
+++ q.php
@@ -2,32 +2,32 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>quizzy</title>
+    <title>quizzical</title>
     <link type="text/css" href="q.css" rel="stylesheet">
 </head>
 <body>
 <section id="main">
-    <h1><a href="<?php printf($_SERVER['PHP_SELF']) ;?>">quizzy</a></h1>
+    <h1><a href="<?php printf($_SERVER['PHP_SELF']) ;?>"><span id="q">quiz</span>zical</a></h1>
 
     <select id="quiz_selector">
         <?php
         printf ( '<option value="0">%1$s</option>', 'choose a quiz') ;
         foreach ( glob('*.json') as $quiz_file ) {
-	        $selected = ( isset ( $_GET['quizzy'] ) ) && ( $_GET['quizzy'] == basename ( $quiz_file, '.json' ) ) ? 'selected' : '' ;
+	        $selected = ( isset ( $_GET['quiz'] ) ) && ( $_GET['quiz'] == basename ( $quiz_file, '.json' ) ) ? 'selected' : '' ;
             printf ( '<option value="%1$s" %2$s>%1$s</option>', basename ( $quiz_file, '.json' ), $selected) ;
 
         }
         ?>
     </select>
     <?php
-    if ( isset ( $_GET['quizzy'] ) ) :
+    if ( isset ( $_GET['quiz'] ) ) :
 
-    if ( ! file_exists($_GET['quizzy'] . '.json') ) {
-        printf ('<p>Sorry, no quiz by the name <strong>%s</strong>!</p>', $_GET['quizzy']) ;
+    if ( ! file_exists($_GET['quiz'] . '.json') ) {
+        printf ('<p>Sorry, no quiz by the name <strong>%s</strong>!</p>', $_GET['quiz']) ;
 	    print_footer ();
         die() ;
     }
-    $json = json_decode ( file_get_contents ($_GET['quizzy'] . '.json' ) ) ;
+    $json = json_decode ( file_get_contents ($_GET['quiz'] . '.json' ) ) ;
     //var_dump($json);
 
     $quiz = $json->quizzes[0] ;
@@ -41,13 +41,13 @@
 //	    var_dump($question);
         printf ('<div id="quiz_body">') ;
         printf ('<div class="question_number"><h3>%1$02d.</h3></div><div class="question_details"><h3>%2$s</h3>', $question_counter+1, $question->question_text ) ;
-        printf ('<ul class="answers">') ;
+        printf ('<div class="answers">') ;
         $answer_counter = 0 ;
         foreach ( $question->answers as $answer) {
-            printf('<li class="question%2$s answer%3$s %4$s">%1$s</li>',htmlentities($answer->answer_text), $question_counter, $answer_counter, $question->correct_answer == $answer_counter ? 'is_correct' : '' ) ;
+            printf('<div class="question%2$s answer%3$s %4$s answer">%1$s</div><div class="icon">&nbsp;</div>',htmlentities($answer->answer_text), $question_counter, $answer_counter, $question->correct_answer == $answer_counter ? 'is_correct' : '' ) ;
             $answer_counter++ ;
         }
-        printf('</ul>') ;
+        printf('</div>') ;
         printf('</div>') ;
         printf('</div>') ;
         $question_counter++ ;
